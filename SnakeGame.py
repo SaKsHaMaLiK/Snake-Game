798b1599cbcc9dabb9cbf65becda2964db29b21b
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-29 21:59:42 +0530

    Add files via upload

---
+++ SnakeGame.py
@@ -0,0 +1,24 @@
+import mathimport random
+import pygame
+import tkinter as tkinter
+from tkinter import messagebox
+
+
+class cube(object)
+
+
+rows = 0
+w = 0
+   def _init_(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class snake(object):
+    def _init_(self, color, pos):
+        pass
