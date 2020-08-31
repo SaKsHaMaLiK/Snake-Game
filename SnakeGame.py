3b62e07dfa22578bb36341b4aeae3966983683cf
Saksham Malik <54699504+SaKsHaMaLiK@users.noreply.github.com>
2020-08-31 10:06:27 +0530

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -161,5 +161,35 @@ def message_box(subject, content):
     expect:
         pass
 
+def main():
+    global width, rows, s, snack
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.display.set_mode((width, height))
+    s = snake((255,0,0), (10,10))
+    snack = cube(randomSnack(rows, s), color=(0,255,0))
+    flag =True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        s.move()
+        if s.body[0].pos == snacl.pos:
+            s.addCube()
+            snack = cube(randomSnack(rows, s), color=(0,255,0))
+
+            for x in range(len(s,body)):
+                if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                    print('Score: 'len(s.body))
+                    message_box('You Lost!','Play again...')
+                    s.reset((10,10))
+                    break
+
+        redrawWindow(win)
+
+    pass
 
 main()
